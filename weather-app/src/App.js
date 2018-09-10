f72f3b564df2fb5e325120d3500a43cc66591fed
DESKTOP-UKLKRC8\IMAGINAMOS <mest.sal18@gmail.com>
2018-09-10 13:01:23 -0500

    updates

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -12,11 +12,33 @@ const API_KEY = "4d9a86a353daf89c397e353effdc0d9f";
 
 class App extends Component {
 
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
 getWeather = async (e) => {
   e.preventDefault()
-  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=München,DE&appid=${API_KEY}&units=metric`)
+
+  const city = e.target.elements.city.value;
+  const country = e.target.elements.country.value;
+
+  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
   const data = await api_call.json()
   console.log(data);
+
+  this.setState({
+    temperature: data.main.temp,
+    city: data.name,
+    country: data.sys.country,
+    humidity: data.main.humidity,
+    description: data.weather[0].description,
+    error: ""
+  })
   
 }
 
